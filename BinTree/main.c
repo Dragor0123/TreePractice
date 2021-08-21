ec4d4e638814f159622c36abe213baa275e3bb63
dragor0123 <blader137@gmail.com>
2021-08-22 00:36:42 +0900

    add main.c

---
+++ BinTree/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file		main.c
+ * @author		blader137 (https://github.com/Dragor0123)
+ * @brief		main.c of BinTree project
+ * @version		0.1
+ * @date		2021-08-22
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	return 0;
+}
